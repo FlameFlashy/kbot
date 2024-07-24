1d40b97cd519960af45283ad6c3c71a2f811201b
Dmytro Sokolov <45371651+FlameFlashy@users.noreply.github.com>
2024-07-24 17:14:31 +0300

    upd jenkins

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -4,11 +4,10 @@ pipeline {
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Choose OS')
         choice(name: 'TARGETOSARCH', choices: ['amd64', 'arm64'], description: 'Choose architecture')
     }
-    environment{
+    environment {
         REPO = 'https://github.com/flameflashy/kbot'
         BRANCH = 'main'
         REGISTRY = 'dmitriysokolov'
-
     }
     stages {
         stage('clone') {
@@ -22,6 +21,7 @@ pipeline {
                 echo 'run test'
                 sh 'make test'
             }
+        }
         stage('build') {
             parallel {
                 stage('Build Linux') {
@@ -39,7 +39,7 @@ pipeline {
                     }
                 }
                 stage('Build Windows') {
-                    when { expression { params.OS == 'windows'  || params.OS == 'all' } }
+                    when { expression { params.OS == 'windows' || params.OS == 'all' } }
                     steps {
                         echo 'Building for Windows'
                         sh 'make image TARGETOS=windows'
@@ -52,30 +52,30 @@ pipeline {
                 stage('Push Linux to dockerhub') {
                     when { expression { params.OS == 'linux' || params.OS == 'all' } }
                     steps {
-                        script{
-                            docker.withRegistry('','dockerhub'){
+                        script {
+                            docker.withRegistry('', 'dockerhub') {
                                 sh 'make push TARGETOS=linux'
-                            }   
+                            }
                         }
                     }
                 }
                 stage('Push Darwin to dockerhub') {
                     when { expression { params.OS == 'darwin' || params.OS == 'all' } }
                     steps {
-                        script{
-                            docker.withRegistry('','dockerhub'){
+                        script {
+                            docker.withRegistry('', 'dockerhub') {
                                 sh 'make push TARGETOS=macos'
-                            }   
+                            }
                         }
                     }
                 }
                 stage('Push Windows to dockerhub') {
                     when { expression { params.OS == 'windows' || params.OS == 'all' } }
                     steps {
-                        script{
-                            docker.withRegistry('','dockerhub'){
+                        script {
+                            docker.withRegistry('', 'dockerhub') {
                                 sh 'make push TARGETOS=windows'
-                            }   
+                            }
                         }
                     }
                 }
