8ced0a65f92d14ed165b0d7474de32ae1a77a09e
Dmytro Sokolov <45371651+FlameFlashy@users.noreply.github.com>
2024-07-24 16:59:17 +0300

    add jenkins file

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 APP=$(shell basename $(shell git remote get-url origin))
 #REGISTRY=ghcr.io/flameflashy
-REGISTRY := dmitriysokolov
+REGISTRY ?= flameflashy
 VERSION=$(shell git describe --tags --abbrev=0)$(shell git rev-parse --short HEAD)
-TARGETOS=linux #linux darwin windows
-TARGETARCH=amd64 #arm64
+TARGETOS ?=linux
+TARGETARCH ?=arm64 
 
 format:
 	gofmt -s -w ./
@@ -21,21 +21,21 @@ build: format get
 	CGO_ENABLED=0 GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build -v -o kbot -ldflags "-X="github.com/flameflashy/kbot/cmd.appVersion=${VERSION}
 
 image:
-	docker build . -t ${REGISTRY}/${APP}:${VERSION}-${TARGETARCH} --build-arg TARGETOS=${TARGETOS} --build-arg TARGETOSARCH=${TARGETOSARCH} --no-cache
+	docker build . -t ${REGISTRY}/${APP}:${VERSION}-${TARGETOS}-${TARGETOSARCH} --build-arg TARGETOS=${TARGETOS} --build-arg TARGETOSARCH=${TARGETOSARCH} --no-cache
 
 push: 
-	docker push ${REGISTRY}/${APP}:${VERSION}-${TARGETARCH}
+	docker push ${REGISTRY}/${APP}:${VERSION}-${TARGETOS}-${TARGETARCH}
 
 
 clean:
 	rm -rf kbot
-	docker rmi ${REGISTRY}/${APP}:${VERSION}-${TARGETARCH}
+	docker rmi ${REGISTRY}/${APP}:${VERSION}-${TARGETOS}-${TARGETARCH}
 
-linux: TARGETOS = linux
-linux: build image push clean
+linux: # Build for linucx, by default this made for arm64
+	${MAKE} build TARGETOS=linux
 
-windows: TARGETOS = windows
-windows: build image push clean
-
-macos: TARGETOS = darwin
-macos: build image push clean
+windows: # Build for windows, by default this made for arm64
+	${MAKE} build TARGETOS=windows
+	
+macos: # Build for macos, by default this made for arm64
+	${MAKE} build TARGETOS=darwin
---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,85 @@
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Choose OS')
+        choice(name: 'TARGETOSARCH', choices: ['amd64', 'arm64'], description: 'Choose architecture')
+    }
+    environment{
+        REPO = 'https://github.com/flameflashy/kbot'
+        BRANCH = 'main'
+        REGISTRY = 'dmitriysokolov'
+
+    }
+    stages {
+        stage('clone') {
+            steps {
+                echo 'Clone repo'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+        stage('test') {
+            steps {
+                echo 'run test'
+                sh 'make test'
+            }
+        stage('build') {
+            parallel {
+                stage('Build Linux') {
+                    when { expression { params.OS == 'linux' || params.OS == 'all' } }
+                    steps {
+                        echo 'Building for Linux'
+                        sh 'make image TARGETOS=linux'
+                    }
+                }
+                stage('Build Darwin') {
+                    when { expression { params.OS == 'darwin' || params.OS == 'all' } }
+                    steps {
+                        echo 'Building for MacOs'
+                        sh 'make image TARGETOS=macos'
+                    }
+                }
+                stage('Build Windows') {
+                    when { expression { params.OS == 'windows'  || params.OS == 'all' } }
+                    steps {
+                        echo 'Building for Windows'
+                        sh 'make image TARGETOS=windows'
+                    }
+                }
+            }
+        }
+        stage('push') {
+            parallel {
+                stage('Push Linux to dockerhub') {
+                    when { expression { params.OS == 'linux' || params.OS == 'all' } }
+                    steps {
+                        script{
+                            docker.withRegistry('','dockerhub'){
+                                sh 'make push TARGETOS=linux'
+                            }   
+                        }
+                    }
+                }
+                stage('Push Darwin to dockerhub') {
+                    when { expression { params.OS == 'darwin' || params.OS == 'all' } }
+                    steps {
+                        script{
+                            docker.withRegistry('','dockerhub'){
+                                sh 'make push TARGETOS=macos'
+                            }   
+                        }
+                    }
+                }
+                stage('Push Windows to dockerhub') {
+                    when { expression { params.OS == 'windows' || params.OS == 'all' } }
+                    steps {
+                        script{
+                            docker.withRegistry('','dockerhub'){
+                                sh 'make push TARGETOS=windows'
+                            }   
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
